0df92af287c903e2bd6de37deb956744ffdbbaf4
raunakcy <raunaktiwari468@gmail.com>
2023-07-02 14:57:49 +0530

    Quiz Application

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,7 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+    public static String question[] ={"Which company owns android?","Which is not the programming language?","Where are you watching this video?","Which country has the highest technology?","Largest country in the world?"};
+    public static String answers[][]={{"Google","Apple","Nokia","Samsung"},{"Java","Kotlin","Notepad","Python"},{"Facebook","Whatsapp","Instagram","You Tube"},{"India","China","Japan","Nepal"},{"Japan","Bhutan","India","Russia"}};
+    public static String correctAnswer[]={"Google","Notepad","You Tube","China","Russia"};
+}
